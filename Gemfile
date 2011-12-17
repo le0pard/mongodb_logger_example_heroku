f77806013e4bdb5c5ddee490670894d9a84c36a8
Alexey <leopard.not.a@gmail.com>
2011-12-17 14:09:19 +0200

    new app

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'rails', '3.1.3'
 gem 'sqlite3'
 gem 'mongodb_logger', '~> 0.2.3'
 gem 'json'
+gem 'pg'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ExampleMongodbLogger::Application.routes.draw do
+  
+  resources :tests
+  
+  root :to => 'tests#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
