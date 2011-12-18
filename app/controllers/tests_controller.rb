370dc830304b6ce2dc040e0a1540ae97a98094bd
Alexey <leopard.not.a@gmail.com>
2011-12-18 20:05:22 +0200

    flash messages

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -6,10 +6,12 @@ class TestsController < ApplicationController
   
   def new
     Rails.logger.add_metadata(:additional_data => {:string => "some_data", :integer => 1, :boolean => true}) if Rails.logger.respond_to?(:add_metadata)
+    flash[:notice] = "Information into log added. Check it on the web."
     redirect_to root_path
   end
   
   def create
+    flash[:notice] = "Information into log added. Check it on the web."
     redirect_to root_path
   end
   
