61c6b4b07a7dbafd5a370e53d14258c21f9c8ff6
Alexey <leopard.not.a@gmail.com>
2012-04-22 22:24:54 +0300

    bump v0.2.8

--- config/initializers/mongodb_logger.rb
+++ config/initializers/mongodb_logger.rb
@@ -1,4 +1,16 @@
 require 'mongodb_logger/server' # required
+
+# callback for errors
+
+MongodbLogger::Base.configure do |config|
+  config.on_log_exception do |mongo_record|
+    STDOUT.puts "##################### Error begin ###########################"
+    STDOUT.puts mongo_record.inspect
+    STDOUT.puts "#####################  Error end  ###########################"
+  end
+end
+
+
 # this secure you web interface by basic auth, but you can skip this, if you no need this
 MongodbLogger::Server.use Rack::Auth::Basic do |username, password|
   [username, password] == ['demo', 'password']
