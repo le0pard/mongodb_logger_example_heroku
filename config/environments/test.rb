662a3b67d177ad48acc51d13670197e75c386744
Alexey Vasiliev <leopard.not.a@gmail.com>
2014-12-08 12:34:04 +0200

    new rails

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -35,6 +35,4 @@ ExampleMongodbLogger::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
-  # Raise exception on mass assignment protection for Active Record models
-  config.active_record.mass_assignment_sanitizer = :strict
 end
